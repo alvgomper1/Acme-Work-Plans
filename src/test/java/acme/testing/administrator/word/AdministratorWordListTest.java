5028d3f0ea45124c1d222f4a5bcd6f1554280533
vicdiacor <vicdiacor@alum.us.es>
2021-05-18 18:32:15 +0200

    Task 010: AdministratorWordList Header of the tests

--- src/test/java/acme/testing/administrator/word/AdministratorWordListTest.java
+++ src/test/java/acme/testing/administrator/word/AdministratorWordListTest.java
@@ -26,6 +26,9 @@ public class AdministratorWordListTest extends AcmePlannerTest {
 	
 	// Test cases -------------------------------------------------------------
 	
+	/* In this test the following feature will be tested: Administrator Word List and Administrator Word Show (Positive test)*/
+	/* All words in CSV are the same as the one shown in the list on the page. Each word in the list will match every word in the CSV, and when we click on a word
+	 * on a word, the InputField to Update it, will match with the word in the CSV too. */
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/administrator/word/list.csv", encoding = "utf-8", numLinesToSkip = 1)
@@ -46,6 +49,10 @@ public class AdministratorWordListTest extends AcmePlannerTest {
 		super.signOut();
 	}
 	
+	
+	/* In this test the following feature will be tested: Administrator Word List (Negative test)*/
+	/* Anonymous rol tries to list spam words, but Anonymous rol is prohibited from performing this action, so a panic exception is expected*/
+	
 	@Test
 	@Order(20)
 	public void AnonymousTryToListWords() {
@@ -55,6 +62,10 @@ public class AdministratorWordListTest extends AcmePlannerTest {
 		
 
 	}
+	
+
+	/* In this test the following feature will be tested: Administrator Word List (Negative test)*/
+	/* Manager rol tries to list spam words, but Manager rol is prohibited from performing this action, so a panic exception is expected*/
 	
 	@Test
 	@Order(30)
