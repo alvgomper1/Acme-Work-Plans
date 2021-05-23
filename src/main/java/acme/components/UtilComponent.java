f86ed207c7e36e3d725d2bee8baf4b07cb1b985b
jvegax <toledo.utrera@hotmail.com>
2021-05-23 16:46:10 +0200

    Se ha modificado la clase test

--- src/main/java/acme/components/UtilComponent.java
+++ src/main/java/acme/components/UtilComponent.java
@@ -1,11 +1,10 @@
 package acme.components;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class UtilComponent {
-
-	
 
 	public static Date addFecha(final Date fechaBase, final int tipoFecha, final int cantidadSumar ) { //Permite sumarle dias, horas o cualquier unidad de tiempo a otra fecha y te devuelve un Date del resultado
 		   final Calendar calendar = Calendar.getInstance();
@@ -13,4 +12,10 @@ public class UtilComponent {
 		      calendar.add(tipoFecha, cantidadSumar);
 		      return calendar.getTime();
 	   }
+	
+	public static String currentDateToString() {
+		final SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+		final Date date = new Date(System.currentTimeMillis());
+		return formatter.format(date);
+	}
 }
--- src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
+++ src/test/java/acme/testing/anonymous/shout/AnonymousShoutCreateTest.java
@@ -6,21 +6,23 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 
+import acme.components.UtilComponent;
 import acme.testing.AcmePlannerTest;
 
 public class AnonymousShoutCreateTest extends AcmePlannerTest{
 
 	
 	/**
-	 * La feature que prueba este test es la de crear un shout sin spam
+	 * La feature que prueba este test es la de crear un shout sin spam ni campos vacios
 	 * <p>
 	 * Para ello accedemos al formulario de creacion de shout.
 	 * Cuando estamos en el formulario de crear shout, se introducen datos de un shout sin spam
 	 */
 	@ParameterizedTest
-	@CsvFileSource(resources = "/anonymous/shout/create-shout-spam-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/anonymous/shout/create-shout-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void createShoutPositive(final String recordIndex, final String author, final String info, final String text) {
+	public void createShoutPositive(final String author, final String info, final String text) {
+		
 		
 		super.clickOnMenu("Anonymous", "Shout!");
 		
@@ -28,13 +30,23 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest{
 		super.fillInputBoxIn("info", info);
 		super.fillInputBoxIn("text", text);
 		
+		final String date = UtilComponent.currentDateToString();
 		super.clickOnSubmitButton("Shout!");
 		
 //		Comprobamos que no hay errores
 		Assertions.assertFalse(super.exists(By.className("text-danger")));
 		
+		super.clickOnMenu("Anonymous", "List of shouts");
+		
+		super.checkColumnHasValue(0, 0, date);
+		super.checkColumnHasValue(0, 1, author);
+		super.checkColumnHasValue(0, 2, text);
+		super.checkColumnHasValue(0, 3, info);
 		
 	}
+	
+	
+	
 	
 	/**
 	 * La feature que prueba este test es la de crear un shout con spam
@@ -44,6 +56,31 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest{
 	 */
 	@ParameterizedTest
 	@CsvFileSource(resources = "/anonymous/shout/create-shout-spam-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void createShoutSpamNegative(final String recordIndex, final String author, final String info, final String text) {
+		
+		super.clickOnMenu("Anonymous", "Shout!");
+		
+		super.fillInputBoxIn("author", author);
+		super.fillInputBoxIn("info", info);
+		super.fillInputBoxIn("text", text);
+		
+		super.clickOnSubmitButton("Shout!");
+		
+		super.checkErrorsExist();
+
+		
+	}
+	
+	
+	/**
+	 * La feature que prueba este test es la de crear un shout con datos vacios o erroneos
+	 * <p>
+	 * Para ello accedemos al formulario de creacion de shout.
+	 * Cuando estamos en el formulario de crear shout, se introducen datos de un shout con datos erroneos
+	 */
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shout/create-shout-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)
 	public void createShoutNegative(final String recordIndex, final String author, final String info, final String text) {
 		
@@ -59,5 +96,9 @@ public class AnonymousShoutCreateTest extends AcmePlannerTest{
 
 		
 	}
+
+	
+	
+	
 	
 }
